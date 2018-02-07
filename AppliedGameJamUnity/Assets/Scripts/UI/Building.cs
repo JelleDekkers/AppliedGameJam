53e28643d4e5250a3876766fa5ce9d85a8ba560f
Cheezegami <superronald@live.nl>
2018-02-07 14:03:54 +0100

    Update

--- AppliedGameJamUnity/Assets/Scripts/UI/Building.cs
+++ AppliedGameJamUnity/Assets/Scripts/UI/Building.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI {
     class Building : ScriptableObject {
         public GameObject prefab;
         public Sprite mask;
+
+        [MenuItem("Assets/Create/Building")]
+        public static void CreateMyAsset() {
+            Building asset = ScriptableObject.CreateInstance<Building>();
+
+            AssetDatabase.CreateAsset(asset, "Assets/ScriptableObjects/NewBuilding.asset");
+            AssetDatabase.SaveAssets();
+
+            EditorUtility.FocusProjectWindow();
+
+            Selection.activeObject = asset;
+        }
     }
 }
--- AppliedGameJamUnity/Assets/Scripts/UI/BuildingList.cs
+++ AppliedGameJamUnity/Assets/Scripts/UI/BuildingList.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI {
     public class BuildingList : MonoBehaviour {
+        public GameObject buttonPrefab;
         public ScriptableObject[] buildingPrefabs;
 
         // Use this for initialization
-        void Start() {
-            
+        private void Start() {
+            for(int i = 0 ; i < buildingPrefabs.Length ; i++) {
+                // CREATE BUTTON WITH SCRIPTABLE OBJECT PROPERTIES
+                GameObject obj = Instantiate<GameObject>(buttonPrefab);
+                obj.transform.parent = gameObject.transform;
+                obj.GetComponent<RectTransform>().position = new Vector2(48 + (i * 100), 48);
+            }
         }
 
         // Update is called once per frame
-        void Update() {
+        private void Update() {
 
         }
     }
