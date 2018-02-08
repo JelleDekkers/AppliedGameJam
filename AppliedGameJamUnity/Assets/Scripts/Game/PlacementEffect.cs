84d5194d00c0a44827757521b9b762c361949ecd
JelleDekkers <jelle.dekkers@hotmail.com>
2018-02-08 19:27:34 +0100

    Added placement effects on placing a building

---
+++ AppliedGameJamUnity/Assets/Scripts/Game/PlacementEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementEffect : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip soundOnPlacement;
+    [SerializeField]
+    private new ParticleSystem particleSystem;
+    [SerializeField]
+    private AudioSource audioSource;
+
+    public void Activate(Vector3 buildingPosition) {
+        audioSource.PlayOneShot(soundOnPlacement);
+        transform.position = buildingPosition;
+        particleSystem.Play();
+    }
+}
